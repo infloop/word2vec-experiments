bb5cb8324cb9467054a62e7ddbf7ec828824b241
infloop <infloop.gm@gmail.com>
2015-10-19 02:24:37 +0400

    Saved all data for further research

--- research.js
+++ research.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var w2v = require( '../lib/main.js' );
+var w2v = require('word2vec');
 
 
 w2v.loadModel(__dirname+ '/output/vectors.txt', function( err, model ) {
     console.log(model);
-    var wordVecs = model.getVectors(["Лукьяненко"]);
-    console.log( model.getNearestWord(wordVecs[0].values, 1) );
-    //var similar = model.mostSimilar("switzerland", 20);
-    //console.log(similar);
+    var wordVecs = model.getVectors(['молчание', 'голосе']);
+    console.log(wordVecs);
+    if(wordVecs) {
+        console.log( model.getNearestWord(wordVecs[0].values, 1) );
+    }
+
+    var similar = model.mostSimilar("молчание", 20);
+    console.log(similar);
     //var analogy = model.analogy("woman",["man", "king"], 10);
     //console.log(analogy);
     //var similarity = model.similarity( 'ham', 'cheese' );
